7348b7428c3b166be73669b3cac5dc0964a981e0
Hasan Ahmed <hasan@boxknight.com>
2019-04-06 18:31:34 -0400

    Basic index.php home page

---
+++ index.php
@@ -0,0 +1,41 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="utf-8" />
+    <meta http-equiv="x-ua-compatible" content="ie=edge" />
+    <meta name="viewport" content="width=device-width, initial-scale=1" />
+
+    <title>Bookstore - COMP 353</title>
+
+    <link rel="stylesheet" href="css/style.css" />
+</head>
+
+<body>
+<h1>Bookstore Web Application</h1>
+
+<ul>
+    <li>
+        <a href="">View All Books</a>
+    </li>
+    <li>
+        <a href="">View all back ordered books</a>
+    </li>
+    <li>
+        <a href="">View special orders by given customer</a>
+    </li>
+    <li>
+        <a href="">View all purchases made by given customer</a>
+    </li>
+    <li>
+        <a href="">View all sales made by a given employee on a specific date.</a>
+    </li>
+    <li>
+        <a href="">Get details of all purchases made. For each customer, return the total
+            amount paid for the books ordered since the beginning of the year.</a>
+    </li>
+    <li>
+        <a href="">List every book ordered but not received within the period set has passed.</a>
+    </li>
+</ul>
+</body>
+</html>
