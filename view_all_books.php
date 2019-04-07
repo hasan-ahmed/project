9a675545573cf00dc1600e6ca85901e1a0251fc0
Hasan Ahmed <hasan@boxknight.com>
2019-04-07 15:23:17 -0400

    Redo table creation

--- view_all_books.php
+++ view_all_books.php
@@ -17,38 +17,43 @@
 ?>
 <?php include "templates/header.php"; ?>
 
-<?php
-    if ($result && $result->num_rows > 0) {
-?>
-    <h2>Results</h2>
-
-    <table>
-        <thead>
+    <h1 align="center">View All Books</h1>
+    <table border="1" align="center" style="line-height:25px;">
+    <tr>
+        <th>ISBN</th>
+        <th>Title</th>
+        <th>Author</th>
+        <th>Edition</th>
+        <th>Price</th>
+        <th>Quantity On Hand</th>
+        <th>Quantity Sold</th>
+        <th>Publisher ID</th>
+    </tr>
+    <?php
+    if($result->num_rows > 0){
+        while($row = $result->fetch_assoc()){
+            ?>
             <tr>
-                <th>ISBN</th>
-                <th>Title</th>
-                <th>Author</th>
-                <th>Edition</th>
-                <th>Price</th>
-                <th>Quantity On Hand</th>
-                <th>Quantity Sold</th>
-                <th>Publisher ID</th>
+                <td><?php echo $row['ISBN']; ?></td>
+                <td><?php echo $row['title']; ?></td>
+                <td><?php echo $row['author']; ?></td>
+                <td><?php echo $row['edition']; ?></td>
+                <td><?php echo $row['price']; ?></td>
+                <td><?php echo $row['quantityOnHand']; ?></td>
+                <td><?php echo $row['quantitySold']; ?></td>
+                <td><?php echo $row['publisherID']; ?></td>
             </tr>
-        </thead>
-        <tbody>
-            <?php while($row = $result->fetch_assoc()) ?>
-            <tr>
-                <td><?php echo $row["ISBN"]; ?></td>
-                <td><?php echo escape($row["title"]); ?></td>
-                <td><?php echo escape($row["author"]); ?></td>
-                <td><?php echo escape($row["edition"]); ?></td>
-                <td><?php echo escape($row["price"]); ?></td>
-                <td><?php echo escape($row["quantityOnHand"]); ?></td>
-                <td><?php echo escape($row["quantitySold"]); ?></td>
-                <td><?php echo escape($row["publisherID"]); ?></td>
-            </tr>
-        </tbody>
-    </table>
-    <?php } ?>
+            <?php
+        }
+    }
+    else
+    {
+        ?>
+        <tr>
+            <th colspan="2">No Books Found</th>
+        </tr>
+        <?php
+    }
+    ?>
 <a href="index.php">Back to home</a>
 <?php include "templates/footer.php"; ?>
