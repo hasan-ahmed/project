d3119ebdb938212fdbdb680ba60cf4ce072a18b0
Hasan Ahmed <hasan@boxknight.com>
2019-04-08 19:24:21 -0400

    Commit

--- custom_query.php
+++ custom_query.php
@@ -4,7 +4,7 @@ if (isset($_POST['submit'])) {
     require "common.php";
 
     $mysqli = new mysqli($hostname, $username, $password, $dbname);
-    $queryText = $_POST['customquery'];
+    $queryText = '"'.$_POST['customquery'].'"';
 
     $result = $mysqli -> query($queryText);
     $row = $result -> fetch_assoc();
